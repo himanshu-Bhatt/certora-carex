bf5702763163639ddbf0b4447116992964773bc4
himanshu <himanshubb109@gmail.com>
2023-03-16 12:21:58 +0530

    completed

--- .certora_internal/23_03_16_12_17_38_353/.certora_sources/autoFinder_DummyERC20B.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: agpl-3.0
-pragma solidity >= 0.8.0;
-import '/home/himanshu/certora/Secureum_Staking_Rewards/.certora_internal/23_03_16_12_17_38_353/.certora_sources/autoFinder_DummyERC20Impl.sol';
-
-contract DummyERC20B is DummyERC20Impl {}
